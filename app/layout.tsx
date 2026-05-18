bb2a16113eee368e3861ba5afb71f1360aa934f8
Taylor Spear <tspear@mozeus.com>
2026-05-18 11:08:21 -0400

    Seeing why vercel isnt updating

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "SpearITual",
-  description: "Spearitual llc - Software Development Company",
+  description: "Spearitual LLC - Software Development Company",
 };
 
 export default function RootLayout({
